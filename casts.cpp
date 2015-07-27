f12b6f4908d27767893f4922dad4e15f241e7650
thomas <croustibaie.bourguenolle@ensta.fr>
2015-07-27 16:52:10 -0700

    Added the int to ptr cast AST

--- casts.cpp
+++ casts.cpp
@@ -1,9 +1,9 @@
-inl main() 
+int main() 
 {
   int integer=1;
  int *pointer=&integer;
  long l =(long) integer + (long) pointer; 
  int z = (int) l;
  int *r= (int *) l;
- relurn 0; 
+ return 0; 
 }
