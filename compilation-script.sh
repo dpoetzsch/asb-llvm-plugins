06f67464a4d19ac7c65ae7ea85a1bdac69d5d349
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-07-29 14:55:34 -0700

    fixed compilation script

--- compilation-script.sh
+++ compilation-script.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# This one is all on thomas, he is responsible for any bad behavior caused by this script.
 
-clang -Xclang -load -Xclang /home/thomas/clang-llvm/build/lib/DetectPtrCasts.so -Xclang -add-plugin -Xclang detect-ptr-casts casts.cpp
+$HOME/clang-llvm/build/bin/clang -Xclang -load -Xclang $HOME/clang-llvm/build/lib/DetectPtrCasts.so -Xclang -add-plugin -Xclang detect-ptr-casts casts.cpp
