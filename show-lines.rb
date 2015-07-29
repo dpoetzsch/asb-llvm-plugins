f9dbfa51e1e9e14f461ef486abf29476b93766ad
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-07-29 10:41:59 -0700

    improved show-lines output

--- show-lines.rb
+++ show-lines.rb
@@ -1,5 +1,36 @@
 #!/usr/bin/env ruby
 
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+
 files = Dir["**/*.c"]
 
 ARGF.read.split("\n").each do |line|
@@ -8,8 +39,11 @@ ARGF.read.split("\n").each do |line|
     puts line
 
     file = files.find { |f| f.end_with? $2 }
-    l = $1.to_i
-    puts File.read(file).split("\n")[l-3..l+3]
+    l = $1.to_i - 1
+    flines = File.read(file).split("\n")
+    puts flines[l-3..l-1]
+    puts flines[l].yellow
+    puts flines[l+1..l+3]
     puts
   end
 end
