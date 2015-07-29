aeac3f11c563715d06013ba85ebedb4314b94b22
thomas <croustibaie.bourguenolle@ensta.fr>
2015-07-29 10:26:14 -0700

    added space in show-lines.rb

--- show-lines.rb
+++ show-lines.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
 files = Dir["**/*.c"]
-
 File.read(ARGV[0]).split("\n").each do |line|
-  if line =~ /cast at(\d+):.+in file: (.+\.c)/
+  if line =~ /cast at (\d+):.+in file: (.+\.c)/
     #puts "#$2 line #$1"
     puts line
 
     file = files.find { |f| f.end_with? $2 }
     l = $1.to_i
-    puts File.read(file).split("\n")[l-3..l+3]
+   # puts File.read(file).split("\n")[l-3..l+3]
     puts
   end
 end
