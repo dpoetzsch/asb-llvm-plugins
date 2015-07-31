97fae12660009c87833f8cf52890ac75d612e099
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-07-31 13:30:29 -0700

    only show each line once

--- show-lines.rb
+++ show-lines.rb
@@ -62,40 +62,52 @@ end
 totalcasts = 0
 icecasts = 0
 
+cast_lines = {}
+
 ARGF.read.split("\n").each do |line|
   if line =~ /cast at\s?(\d+):.+in file: (.+\.\w+)/
-    totalcasts += 1
-    l = $1.to_i - 1
+    lineno = $1.to_i - 1
     filename = $2
     
     while filename =~ /^\.\.?\/(.+)/
       filename = $1
     end
+    
+    if cast_lines.has_key? filename
+      cast_lines[filename][lineno] = line
+    else
+      cast_lines[filename] = {lineno => line}
+    end
+  end
+end
+
+cast_lines.each do |filename, lines|
+  files = allfiles.find_all { |f| f.end_with?(filename) and File.basename(f) == File.basename(filename) }
   
-    isice = line.include?("constant")
+  lines.each do |lineno, msg|
+    totalcasts += 1
+    isice = msg.include?("constant")
     if isice
       icecasts += 1
     end
     
     if not noconst or not isice
-      puts line
+      puts msg
 
       if $showval > 0
-        files = allfiles.find_all { |f| f.end_with?(filename) and File.basename(f) == File.basename(filename) }
-        
         if files.empty?
           puts "File not found"
         elsif files.length == 1
           flines = File.read(files[0]).split("\n")
-          put_ptr_cast(flines, l)
+          put_ptr_cast(flines, lineno)
         else
           puts "Found  #{files.length} files; guessing correct one"
           
           files.each do |file|
             flines = File.read(file).split("\n")
             
-            if is_pointer_cast_line?(flines[l])
-              put_ptr_cast(flines, l)
+            if is_pointer_cast_line?(flines[lineno])
+              put_ptr_cast(flines, lineno)
             end
           end
         end
