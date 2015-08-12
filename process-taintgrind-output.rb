9b0fb55450ebd8185f1ec4422b4e3f87e9a088a8
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-08-11 17:16:45 -0700

    added basic processing of taintgrind output

---
+++ process-taintgrind-output.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require_relative "colored_str.rb"
+
+class TaintGrindOp
+  def self.is_taintgrindop_line?(line)
+    return line.split(" | ").length == 5
+  end
+  
+  def initialize(line)
+    elems = line.split(" | ")
+    if elems[0] =~ /0x\w+: (\w+) \((.+):(\d+)\)/  # e.g. 0x40080D: main (two-taints.c:10)
+      @func = $1
+      @file = $2
+      @lineno = $3.to_i
+    end
+    
+    if elems[4] =~ /^(.+?) <- (.+?)$/ # e.g. t54_1741 <- t42_1773, t29_4179
+      @var = $1
+      @from = $2.split(", ")
+    else  # e.g. t54_1741
+      @var = elems[4]
+      @from = []
+    end
+    
+    if elems[1].start_with? "IF "
+      @is_sink = true
+    else
+      @is_sink = false
+    end
+    
+    @preds = []
+  end
+  
+  def to_s
+    return "#@func (#@file:#@lineno)" 
+  end
+  
+  attr_reader :func, :file, :lineno, :var, :from, :preds, :is_sink
+end
+
+###### CREATE TaintGrindOp GRAPH ##########
+
+# var -> [TaintGrindOp]
+taintgrind_ops = {}
+sinks = []
+
+ARGF.read.split("\n").each do |line|
+  if not TaintGrindOp.is_taintgrindop_line? line
+    next
+  end
+  
+  tgo = TaintGrindOp.new(line)
+  
+  # link to predecessors
+  tgo.from.each do |fromvar|
+    if taintgrind_ops.has_key?(fromvar)
+      tgo.preds.concat(taintgrind_ops[fromvar])
+    end
+  end
+  
+  if taintgrind_ops.has_key?(tgo.var)
+    taintgrind_ops[tgo.var].push(tgo)
+  else
+    taintgrind_ops[tgo.var] = [tgo]
+  end
+  
+  if tgo.is_sink
+    sinks.push tgo
+  end
+end
+
+puts sinks
