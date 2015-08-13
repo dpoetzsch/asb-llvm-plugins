12dd7aad11bfbdad52b9bf6227f3b7edc7e0bb09
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-08-12 17:15:39 -0700

    improved taintgrind processing

--- process-taintgrind-output.rb
+++ process-taintgrind-output.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "set"
 require_relative "colored_str.rb"
 require_relative "util.rb"
 
@@ -13,7 +14,7 @@ class TaintGrindOp
     @is_sink = false
   
     elems = line.split(" | ")
-    if elems[0] =~ /0x\w+: (\w+) \((.+):(\d+)\)/  # e.g. 0x40080D: main (two-taints.c:10)
+    if elems[0] =~ /0x\w+: (.+?) \((.+):(\d+)\)/  # e.g. 0x40080D: main (two-taints.c:10)
       @func = $1
       @file = $2
       @lineno = $3.to_i
@@ -41,35 +42,76 @@ class TaintGrindOp
   def is_tmp_var
     return @var =~ /^t\d+_\d+$/
   end
+  
+  def is_use?
+    return @from.empty?
+  end
+  
+  def is_def?
+    return (not self.is_use?)
+  end
+  
+  def get_src_lines
+    return guess_path(@file).map { |f| File.read(f).split("\n")[@lineno-1] }.find_all { |l| not l.nil? }
+  end
 
   def to_s
-    lines = guess_path(@file).map { |f| File.read(f).split("\n")[@lineno-1] }.find_all { |l| not l.nil? }
+    puts
+    puts @line
+    lines = self.get_src_lines()
     
     line = lines[0]
-    line = line.red if self.is_sink
+    line = line.red if self.is_sink and not line.nil?
     
     s = "#@func (#@file:#@lineno):  #{line}"
-    s += " (found #{lines.length} matching files}" if lines.length > 1
+    s += " (found #{lines.length} matching files)" if lines.length > 1
     return s
   end
   
   def get_full_path
-    p = @preds.empty? ? [] : @preds.map{|op| op.get_full_path()}.flatten
-    return p.push self
+    # we can't use recursion here because the graph can be VERY huge and the
+    # stack depth is just not enough for that
+    path = []
+    stack = [self]
+    #processed = Set.new
+    
+    while not stack.empty?
+      op = stack.pop
+      #next if processed.include? op
+      #processed.add op
+      
+      path.push(op) if not block_given? or yield op
+      stack.concat op.preds
+    end
+    
+    return path.reverse
   end
   
   def get_path
-    p = @preds.empty? ? [] : @preds.map{|op| op.get_path()}.flatten
-    p.push self if self.is_sink or not self.is_tmp_var
-    return p
+    return get_full_path { |op| (op.is_sink or not op.is_tmp_var) and (not block_given? or yield op) }
   end
   
-  attr_reader :func, :file, :lineno, :var, :from, :preds, :is_sink
+  def get_path_unique
+    locs = Set.new
+    return get_full_path { |op|
+      loc = [op.file, op.lineno]
+      ret = locs.include?loc
+      locs.add loc
+      p loc if not ret
+      not ret
+    }
+  end
+  
+  def get_path_no_lib
+    return get_path { |op| not op.get_src_lines.empty? }
+  end
+  
+  attr_reader :func, :file, :lineno, :var, :from, :preds, :is_sink, :line
 end
 
 ###### CREATE TaintGrindOp GRAPH ##########
 
-# var -> [TaintGrindOp]
+# var -> TaintGrindOp
 taintgrind_ops = {}
 sinks = []
 
@@ -83,17 +125,18 @@ ARGF.read.split("\n").each do |line|
   # link to predecessors
   tgo.from.each do |fromvar|
     if taintgrind_ops.has_key?(fromvar)
-      tgo.preds.concat(taintgrind_ops[fromvar])
+      tgo.preds.push(taintgrind_ops[fromvar])
     end
   end
-  if taintgrind_ops.has_key? tgo.var
-    tgo.preds.concat taintgrind_ops[tgo.var]
+  if tgo.is_use? and taintgrind_ops.has_key? tgo.var
+    tgo.preds.push taintgrind_ops[tgo.var]
   end
   
-  if taintgrind_ops.has_key?(tgo.var)
-    taintgrind_ops[tgo.var].push(tgo)
-  else
-    taintgrind_ops[tgo.var] = [tgo]
+  if tgo.is_def?
+    if taintgrind_ops.has_key?(tgo.var)
+      puts "ERROR: Duplicated definition"
+    end
+    taintgrind_ops[tgo.var] = tgo
   end
   
   if tgo.is_sink
@@ -105,4 +148,5 @@ sinks.each do |sink|
   puts ">>>> The evil cast should occur just before that <<<<"
   puts sink.get_path
   puts "="*40
+  break  # TODO REMOVE THIS
 end
