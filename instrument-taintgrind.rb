2f13759e4b9e76f757964030358de0af4d058d4b
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-08-13 15:07:33 -0700

    fixing taintgrind instrumentation

--- instrument-taintgrind.rb
+++ instrument-taintgrind.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
-require_relative"util.rb"
 
+require_relative "util.rb"
+require_relative "colored_str.rb"
 
 $count=0
 
@@ -33,78 +34,88 @@ def find_cast_var(line) #Not used, except for debugging
   end
 end
 
-def rewrite_source(filename, line)
-  file = File.read(filename).split("\n") 
-  linenumber= line.to_i
+def rewrite_source(filename, linenumber, colno)
+  lines = File.read(filename).split("\n")
   tmpname=get_new_name
-  if file[linenumber]=~/(.+) \(([A-Za-z0-9_]+)\) ([A-Za-z0-9_]+)(.+)/
-    file[linenumber]=$1 + " " + tmpname + $4
-    type = $2
-    varname= $3
-    puts "found cast in #{filename} line #{line}, #{varname} is cast to #{type}"
+  
+  prefix = lines[linenumber][0...colno]
+  
+  if lines[linenumber][colno..-1] =~ /^\(([A-Za-z0-9_]+)\) ([A-Za-z0-9_]+)(.+)/
+    lines[linenumber]= prefix + " " + tmpname + $3
+    type = $1
+    varname= $2
+    cast = "(#{type}) #{varname}"
+    puts "Found cast in #{filename} at #{linenumber}:#{colno}: #{cast.red}; Replacing:"
+    
+    lines.insert(linenumber,"#{type} #{tmpname} = (#{type}) #{varname};")
+    lines.insert(linenumber+1, "TNT_MAKE_MEM_TAINTED(&#{tmpname}, sizeof(#{tmpname}));")
+    
+    puts lines[linenumber..linenumber+2]
+    puts 
+    File.open(filename, "w") {|f| f.write(lines.join("\n"))}
   else
-    puts "Can't find the cast" 
-    return nil
+    puts "Can't find the cast"
   end
-  file.insert(linenumber,"#{type} #{tmpname} = #{type} #{varname};")
-  file.insert(linenumber+1, "TNT_MAKE_MEM_TAINTED(&#{tmpname},sizeof(#{tmpname}));")
-  File.open(filename, "w") {|f| f.write(file.join("\n"))}
 end
 
 def add_header(filename)
-  file = File.read(filename).split("\n")
+  lines = File.read(filename).split("\n")
   header_included=0
-  file.each do |line|
+  lines.each do |line|
     if line == "#include \"taintgrind.h\""
       header_included=1
       break
     end
   end
   if header_included == 0
-    puts "no taingrind header"
-    file.insert(0,"#include \"taintgrind.h\"")
-    File.open(filename, "w") {|f| f.write(file.join("\n"))}
+    puts "adding #include \"taintgrind.h\""
+    lines.insert(0,"#include \"taintgrind.h\"")
+    File.open(filename, "w") {|f| f.write(lines.join("\n"))}
   end
 end
 
 cast_lines = {}
 
 ARGF.read.split("\n").each do |line|
-  if line =~ /cast at\s?(\d+):.+in file: (.+\.\w+)/
+  if line =~ /cast at\s?(\d+):(\d+).+?in file: (.+\.\w+)/
     lineno = $1.to_i - 1
-    filename = $2
+    colno = $2.to_i - 1
+    filename = $3
     
     while filename =~ /^\.\.?\/(.+)/ #cuts the ../../filename.c to filename.c
       filename = $1
     end
+    
     if cast_lines.has_key? filename
-      cast_lines[filename][lineno] = line
+      cast_lines[filename][[lineno, colno]] = line
     else
-      cast_lines[filename] = {lineno => line}
+      cast_lines[filename] = {[lineno, colno] => line}
     end
   end
 end
 
 cast_lines.each do |filename, lines|
   files = guess_path(filename)
-  lines.each do |lineno,msg|
-   
+  lines.each do |linecol,msg|
+    lineno, colno = linecol
     if files.empty?
       puts "file #{filename} not found"
       return nil
-    elsif files.length>1
+    else
       puts "found #{files.length} files; guessing correct one"
       files.each do |file|
         flines=File.read(file).split("\n")
         if is_pointer_cast_line?(flines[lineno]) #We use the first file including a cast
-          files[0]=file
+          rewrite_source(file,lineno, colno)
+          break
         end
       end
     end
-    rewrite_source(files[0],lineno)
   end
   
   add_header(files[0])
+  
+  puts "="*80
 end 
 
 
