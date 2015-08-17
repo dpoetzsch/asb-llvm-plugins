38e65971d642e3d5b09d6b7f5232296761cdfbec
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-08-17 13:32:21 -0700

    fixed script for two casts in same line

--- instrument-taintgrind.rb
+++ instrument-taintgrind.rb
@@ -4,6 +4,7 @@ require_relative "util.rb"
 require_relative "colored_str.rb"
 
 $dryrun = false
+$verbose = false
 $count=0
 
 def get_new_name ()
@@ -35,39 +36,58 @@ def find_cast_var(line) #Not used, except for debugging
   end
 end
 
-def rewrite_source(filename, linenumber, colstart, last_token_start)
+def rewrite_source(filename, lineno, all_cols)
   lines = File.read(filename).split("\n")
-  tmpname=get_new_name
   
-  prefix = lines[linenumber][0...colstart]
-  puts "prefix = " + prefix
-  infix = lines[linenumber][colstart..last_token_start]
-  puts "infix = " + infix
-  suffix = lines[linenumber][last_token_start+1..-1]
-  puts "suffix = " + suffix
+  lines_before = lines[0...lineno]
+  castline = lines[lineno]
+  lines_after = lines[lineno+1..-1]
   
-  if not (infix[-1] == "]" or infix[-1] == ")")
-    if suffix=~/^([A-Za-z0-9_]+)(.+)$/
-       infix += $1
-       suffix = $2
+  # start by the last cast
+  all_cols.sort.reverse.each do |cols|
+    colstart, last_token_start = cols
+    
+    prefix = castline[0...colstart]
+    thecast = castline[colstart..last_token_start]
+    suffix = castline[last_token_start+1..-1]
+    
+    if $verbose
+      puts "prefix = " + prefix
+      puts "cast = " + thecast
+      puts "suffix = " + suffix
     end
-  end  
-  if infix =~ /^\(([A-Za-z0-9_]+)\)\s*(.+)$/
-    lines[linenumber]= prefix + " " + tmpname + suffix
-    type = $1
-    varname= $2
-    cast = "(#{type}) #{varname}"
-    puts "Found cast in #{filename} at #{linenumber}:#{colstart}: #{cast.red}; Replacing:".green
     
-    lines.insert(linenumber,"#{type} #{tmpname} = (#{type}) #{varname};")
-    lines.insert(linenumber+1, "TNT_MAKE_MEM_TAINTED(&#{tmpname}, sizeof(#{tmpname}));")
+    if not (thecast[-1] == "]" or thecast[-1] == ")")
+      if suffix=~/^([A-Za-z0-9_]+)(.+)$/
+         thecast += $1
+         suffix = $2
+      end
+    end
     
-    puts lines[linenumber..linenumber+2]
-    puts 
-    File.open(filename, "w") {|f| f.write(lines.join("\n") + "\n") } if not $dryrun
-  else
-    puts "Can't find the cast"
+    if thecast =~ /^\(([A-Za-z0-9_]+)\)\s*(.+)$/
+      puts("Found cast in #{filename} at #{lineno}:#{colstart}-#{last_token_start}: ".green + prefix + thecast.red + suffix)
+    
+      type = $1
+      varname= $2
+      
+      tmpname = get_new_name()
+      
+      # replace cast line
+      castline = prefix + " " + tmpname + " " + suffix
+      
+      lines_before.push "#{type} #{tmpname} = (#{type}) #{varname};"
+      lines_before.push "TNT_MAKE_MEM_TAINTED(&#{tmpname}, sizeof(#{tmpname}));"
+      
+      puts lines_before[-2..-1]
+      puts castline
+      puts
+    else
+      puts "Can't find the cast"
+    end
   end
+  
+  lines = lines_before + [castline] + lines_after
+  File.open(filename, "w") {|f| f.write(lines.join("\n") + "\n") } if not $dryrun
 end
 
 def add_header(filename)
@@ -80,14 +100,11 @@ def add_header(filename)
     end
   end
   if header_included == 0
-    puts "adding #include \"taintgrind.h\""
+    puts "Adding #include \"taintgrind.h\"".yellow
     lines.insert(0,"#include \"taintgrind.h\"")
     File.open(filename, "w") {|f| f.write(lines.join("\n"))} if not $dryrun
   end
 end
-
-cast_lines = {}
-cnt = 0
 
 # process cli arguments
 loop do
@@ -95,10 +112,17 @@ loop do
   when "-dryrun"
     $dryrun = true
     ARGV.shift
+  when "-v"
+    $verbose = true
+    ARGV.shift
   else
     break
   end
 end
+
+# {filename => {lineno => [[colstart, last_token_colstart]]} }
+cast_lines = {}
+cnt = 0
 
 ARGF.read.split("\n").each do |line|
   if line =~ /cast at\s?(\d+):(\d+)-(\d+).+?in file: (.+\.\w+)/
@@ -112,33 +136,38 @@ ARGF.read.split("\n").each do |line|
       filename = $1
     end
     
-    if cast_lines.has_key? filename
-      cast_lines[filename][[lineno, colstart, last_token_start]] = line
-    else
-      cast_lines[filename] = {[lineno, colstart, last_token_start] => line}
+    if not cast_lines.has_key? filename
+      cast_lines[filename] = {}
     end
+    if not cast_lines[filename].has_key? lineno
+      cast_lines[filename][lineno] = []
+    end
+    
+    cast_lines[filename][lineno].push [colstart, last_token_start]
   end
 end
 
 puts "Found #{cnt} casts"
 
-cast_lines.each do |filename, lines|
+cast_lines.each do |filename, linecols|
   puts "="*80
   
   files = guess_path(filename)
-  linecols = lines.keys.sort.reverse
+  lines = linecols.keys.sort.reverse
   
-  linecols.each do |linecol|
-    lineno, colstart, last_token_start = linecol
+  lines.each do |lineno|
     
     if files.empty?
       puts "File #{filename} not found"
     else
       puts "Found #{files.length} files; guessing correct one" if files.length > 1
+      
+      cols = linecols[lineno] # all [startcol, endcol] pairs of casts in this line
+      
       files.each do |file|
         flines=File.read(file).split("\n")
-        if is_pointer_cast_line?(flines[lineno], colstart) #We use the first file including a cast
-          rewrite_source(file,lineno, colstart, last_token_start)
+        if is_pointer_cast_line?(flines[lineno], cols[0][0]) #We use the first file including a cast
+          rewrite_source(file,lineno, cols)
           break
         end
       end
